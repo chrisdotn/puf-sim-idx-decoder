9f64d86380599190f617864ac6b753d7044a2310
Christoph Niemann <christoph@dune.com>
2025-10-29 12:10:00 +0100

    Add deployment blocks for factories
    Signed-off-by: Christoph Niemann <christoph@dune.com>

--- listeners/src/Main.sol
+++ listeners/src/Main.sol
@@ -9,9 +9,12 @@ contract Triggers is BaseTriggers {
     address public constant factoryAddress = 0x3140167E09d3cfB67b151C25d54fa356f644712D;
     address public constant factoryV4Address = 0xc301BaCE6E9409B1876347a3dC94EC24D18C1FE4;
 
+    uint256 public constant factoryDeploymentBlock = 8416059;
+    uint256 public constant factoryV4DeploymentBlock = 19020796;
+
     function triggers() external virtual override {
         Listener listener = new Listener();
-        addTriggers(chainContract(Chains.WorldChain, factoryV4Address), listener.allTriggers());
+        addTriggers(chainContract(Chains.WorldChain, factoryV4Address).withStartBlock(factoryV4DeploymentBlock), listener.allTriggers());
     }
 }
 
